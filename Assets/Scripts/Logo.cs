5b11c462f1db0a27e12fe919aced61c1e30631c2
unknown <jhauser@E1116W06.win.cis.ksu.edu>
2016-02-06 14:19:59 -0600

    Game Start Up Sequence Done
    The start-up sequence for the game, which consists
    of the main menu screen, the logo screen, and the instantiation
    of Level 1 is done. However, the sprites for all of the
    stages specified above need to be updated.

---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo : MonoBehaviour {
+	float initTime;
+
+	// Use this for initialization
+	void Start ()
+	{
+		initTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Transition to logo scene
+		if((Time.time - initTime) > (float)5) {
+			Application.LoadLevel ("Level1");
+		}
+	}
+}
