bbe885accc024ab2ce044d463bba9e7b74f16414
unknown <jhauser@E1116W06.win.cis.ksu.edu>
2016-02-06 12:10:15 -0600

    Create Level 1 And Menu #1
    Initial start to creating level 1 setup, main menu,
    and logo screen. Submitting before done to test git.

---
+++ Assets/Scripts/Germ.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Germ : MonoBehaviour
+{
+	private Rigidbody2D myRigidbody;
+	bool hasInsulin = false;
+	double speed = 2.0;
+
+	// Use this for initialization
+	void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	// Kill the attached insulin
+	void KillInsulin()
+	{
+		if (hasInsulin) {
+			speed = speed * 0.6;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		HandleMovement();
+	}
+
+	// Handle the movement to the left of the screen
+	private void HandleMovement()
+	{
+		myRigidbody.velocity = Vector2.left; // x = -1, y = 0
+	}
+}
---
+++ Assets/Scripts/MainMenuOptions.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuOptions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Determine if the user wants to start the game
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2, 80, 20), "Start"))
+		{
+			Application.LoadLevel("LogoScene");
+		}
+
+		// Determine if the user wants to quit the game
+		if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2 + 100, 80, 20), "Quit"))
+		{
+			Application.Quit();
+		}
+	}
+}
