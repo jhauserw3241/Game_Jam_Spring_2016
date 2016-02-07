2adb97da221f704149bb29c105aca307a9255e83
unknown <jhauser@E1116W06.win.cis.ksu.edu>
2016-02-06 18:29:05 -0600

    Merge branch 'master' of https://github.com/jhauserw3241/Game_Jam_Spring_2016

---
+++ Assets/Scripts/RotateInCircle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateInCircle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Get the current objects tranform properties
+		Transform objTransProps = this.gameObject.transform;
+
+		// Rotate object
+		objTransProps.Rotate(Vector3.forward, 2f, Space.Self);
+	}
+}
